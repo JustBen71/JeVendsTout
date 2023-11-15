eb26b11377dfd903d5e276e34bc5a7ce633835b0
Benjamin <randazzobenjamin@outlook.fr>
2023-11-15 21:05:26 +0100

    Initialisation

---
+++ src/main/java/com/jevendtout/jevendtout/JevendtoutApplication.java
@@ -0,0 +1,13 @@
+package com.jevendtout.jevendtout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JevendtoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JevendtoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jevendtout/jevendtout/JevendtoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jevendtout.jevendtout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JevendtoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
